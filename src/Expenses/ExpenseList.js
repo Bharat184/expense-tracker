a24266489329adbb94cb413b558d843b27978faa
Bharat Chimariya <bharatchimsz@gmail.com>
2023-01-22 18:28:30 +0530

    parsed html entities

--- src/Expenses/ExpenseList.js
+++ src/Expenses/ExpenseList.js
@@ -4,6 +4,16 @@ const ExpenseList = (props) => {
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const date=new Date(props.data.date);
 
+    function decode(str) {
+
+        let txt = document.createElement("textarea");
+        
+        txt.innerHTML = str;
+        
+        return txt.value;
+        
+        }
+
     return (
         <div className={styles.expense_list}>
             <span className={styles.delete_btn} onClick={()=>{deleteExpense(props.data._id)}}></span>
@@ -14,7 +24,7 @@ const ExpenseList = (props) => {
                 <p>{date.getDate()}</p>
             </div>
            
-            <p>{props.data.title}</p>
+            <p>{decode(props.data.title)}</p>
            
             </div>
             <div className={styles.info}>
